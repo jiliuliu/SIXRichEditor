cf5a34790ae4ebe755390e11330c0aa3c9dfe31b
ljl <liujiliu1989@163.com>
2018-07-29 09:42:50 +0800

    Initial Commit

---
+++ SIXRichEditor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SIXRichEditor
+//
+//  Created by 刘吉六 on 2018/7/29.
+//  Copyright © 2018年 liujiliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SIXRichEditor/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SIXRichEditor
+//
+//  Created by 刘吉六 on 2018/7/29.
+//  Copyright © 2018年 liujiliu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
